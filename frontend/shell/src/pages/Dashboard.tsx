c6092269c3f6dd2e0ac9166afbb8d40fa96df828
sileshidev <contactsileshi@gmail.com>
2025-08-14 15:25:38 +0300

    Delete frontend directory

--- frontend/shell/src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard(){
-  return <div style={{padding:16}}>
-    <h1>CMS Dashboard</h1>
-    <ul>
-      <li><a href="/contraband">Contraband</a></li>
-    </ul>
-  </div>
-}
