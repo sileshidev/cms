c7784a433631406a979e04ba1cfe0c9a9af6e609
sileshidev <contactsileshi@gmail.com>
2025-08-14 15:25:30 +0300

    Delete backend directory

--- backend/audit-service/src/main/java/com/cms/audit/service/model/AuditRepository.java
+++
@@ -1,5 +0,0 @@
-package com.cms.audit.service.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditRepository extends JpaRepository<AuditEvent, Long> {}
--- backend/contraband-service/src/main/java/com/cms/contraband/service/model/ContrabandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cms.contraband.service.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ContrabandRepository extends JpaRepository<Contraband, Long> {
-    Optional<Contraband> findByContrabandCode(String contrabandCode);
-}
